8c2fc0356de58f9c7c86ae6e2ff396fbd959143e
Oliver <silver.gw.03@gmail.com>
2026-08-15 15:16:19 +0200

    style: put Github svg

--- space.js
+++ space.js
@@ -1683,7 +1683,11 @@ function showPanel(data) {
       ${data.tags.map((t) => `<span class="pip-tag">${t}</span>`).join("")}
     </div>
     <div class="pip-actions">
-      <a class="pip-btn primary" href="${data.github}" target="_blank" rel="noreferrer">↗ View on GitHub</a>
+      <a class="pip-btn primary" href="${data.github}" target="_blank" rel="noreferrer"> View on GitHub
+        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+          <path d="M12 2C6.477 2 2 6.477 2 12c0 4.42 2.865 8.167 6.839 9.49.5.092.682-.217.682-.483 0-.237-.009-.866-.014-1.7-2.782.604-3.369-1.342-3.369-1.342-.455-1.157-1.11-1.465-1.11-1.465-.908-.621.069-.608.069-.608 1.004.07 1.532 1.032 1.532 1.032.892 1.529 2.341 1.087 2.91.831.091-.646.35-1.087.636-1.338-2.22-.253-4.555-1.11-4.555-4.943 0-1.091.39-1.984 1.029-2.683-.103-.253-.446-1.27.098-2.647 0 0 .84-.269 2.75 1.025A9.564 9.564 0 0 1 12 6.844a9.56 9.56 0 0 1 2.504.337c1.909-1.294 2.748-1.025 2.748-1.025.546 1.377.202 2.394.1 2.647.64.699 1.028 1.592 1.028 2.683 0 3.842-2.338 4.687-4.566 4.935.359.309.678.919.678 1.852 0 1.336-.012 2.415-.012 2.743 0 .269.18.58.688.482A10.001 10.001 0 0 0 22 12C22 6.477 17.523 2 12 2z"/>
+        </svg>
+      </a>
       <button class="pip-btn ghost" type="button" data-action="close">Close &amp; return to orbit</button>
       <button class="pip-btn ghost" type="button" data-action="exit">Back to Traditional Portfolio</button>
     </div>
